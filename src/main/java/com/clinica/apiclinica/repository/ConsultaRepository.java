1301356503b08be1b64bfe6eb918284dfa9284cf
Eric1557 <ericbbarbosa2019@gmail.com>
2026-04-16 15:32:56 -0300

    Classe ConsultaRepository

---
+++ src/main/java/com/clinica/apiclinica/repository/ConsultaRepository.java
@@ -0,0 +1,8 @@
+package com.clinica.apiclinica.repository;
+
+import com.clinica.apiclinica.model.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta,Long> {
+
+}
